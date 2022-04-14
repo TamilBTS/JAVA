84c1514a5e7f6e3e713e08a24e84dc96d1ea9056
Tamilarasi <arasitamil427@gmail.com>
2022-04-14 23:20:07 +0530

    example prgm

---
+++ example.java
@@ -0,0 +1,34 @@
+import org.w3c.dom.ls.LSOutput;
+
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+    class example{
+        public static void main(String[] args) {
+            Random random=new Random();
+            int array[]=new int[5];
+            for(int i=0;i<array.length;i++){
+                array[i]=random.nextInt(10);
+            }
+            System.out.println(Arrays.toString(array));
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter no:");
+            int n=scanner.nextInt();
+            example sample=new example();
+            sample.checking(array,n);
+        }
+        void checking(int a[],int n){
+            for(int i=0;i<a.length;i++){
+                if(a[i]==n) {
+                    System.out.println("index: " + i);
+                    return;
+                }
+
+            }
+            System.out.println("index: -100");
+        }
+
+    }
+}
